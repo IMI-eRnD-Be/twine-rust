dcf5af16d107074c0e473fcd22df7417eec0dfc8
Cecile Tonglet <cecile.tonglet@cecton.com>
2021-02-01 16:51:31 +0100

    Convert to normal function to use dynamically at runtime (#4)

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@
 //! ```no_run
 //! fn main() {
 //!     println!("cargo:rerun-if-changed=build.rs");
-//!     twine::build_translations!("translations.ini" => "i18n.rs");
+//!     twine::build_translations(&["translations.ini"], "i18n.rs");
 //! }
 //! ```
 //!
@@ -88,134 +88,132 @@
 //! This work is dual-licensed under Apache 2.0 and MIT.
 //! You can choose between one of them if you use this work.
 
-#[doc(hidden)]
-pub use heck;
-#[doc(hidden)]
-pub use regex;
-
-use std::collections::HashMap;
+use heck::{CamelCase, SnakeCase};
+use regex::Regex;
+use std::collections::{HashMap, HashSet};
 use std::fs;
 use std::io;
 use std::path::Path;
 
-#[macro_export]
-macro_rules! build_translations {
-    ($($ini_files:expr),+ => $output_file:expr) => {{
-        use ::std::collections::{HashMap, HashSet};
-        use ::std::path::Path;
-        use $crate::heck::{CamelCase, SnakeCase};
+#[allow(clippy::single_char_add_str)]
+pub fn build_translations<P: AsRef<Path>, O: AsRef<Path>>(
+    ini_files: &[P],
+    output_file: O,
+) -> io::Result<()> {
+    // regex that tries to parse printf's format placeholders
+    // see: https://docs.microsoft.com/en-us/cpp/c-runtime-library/format-specification-syntax-printf-and-wprintf-functions?view=msvc-160
+    let re_printf = Regex::new(r"%([-+#])?(\d+)?(\.\d+)?([dis@xXf])|[^%]+|%%|%$").unwrap();
+    let re_lang = Regex::new(r"(\w+)(-(\w+))?").unwrap();
 
-        // regex that tries to parse printf's format placeholders
-        // see: https://docs.microsoft.com/en-us/cpp/c-runtime-library/format-specification-syntax-printf-and-wprintf-functions?view=msvc-160
-        let re_printf = $crate::regex::Regex::new(
-            r"%([-+#])?(\d+)?(\.\d+)?([dis@xXf])|[^%]+|%%|%$"
-        ).unwrap();
-        let re_lang = $crate::regex::Regex::new(r"(\w+)(-(\w+))?").unwrap();
+    // turns all the keys into snake case automatically
+    let normalize_key = |key: &str| key.to_snake_case().replace(".", "__");
 
-        // turns all the keys into snake case automatically
-        let normalize_key = |key: &str| {
-            key.to_snake_case().replace(".", "__")
-        };
-
-        // generate match arms for each language (for a given translation key)
-        let generate_match_arms = |
-            translations: HashMap<String, String>,
-            all_languages: &mut HashSet<String>,
-        | {
-            let mut match_arms = Vec::new();
-            for (lang, text) in translations {
-                // transform all printf's format placeholder to Rust's format
-                let mut out = String::new();
-                for caps in re_printf.captures_iter(text.as_str()) {
-                    if let Some(type_) = caps.get(4) {
-                        out.push_str("{:");
-                        if let Some(flag) = caps.get(1) {
-                            out.push_str(flag.as_str());
-                        }
-                        if let Some(width) = caps.get(2) {
-                            out.push_str(width.as_str());
-                        }
-                        if let Some(precision) = caps.get(3) {
-                            out.push_str(precision.as_str());
-                        }
-                        match type_.as_str() {
-                            x @ "x" | x @ "X" => out.push_str(x),
-                            _ => {},
-                        }
-                        out.push_str("}");
-                    } else if &caps[0] == "%%" {
-                        out.push_str("%");
-                    } else {
-                        out.push_str(&caps[0]);
+    // generate match arms for each language (for a given translation key)
+    let generate_match_arms = |translations: HashMap<String, String>,
+                               all_languages: &mut HashSet<String>| {
+        let mut match_arms = Vec::new();
+        for (lang, text) in translations {
+            // transform all printf's format placeholder to Rust's format
+            let mut out = String::new();
+            for caps in re_printf.captures_iter(text.as_str()) {
+                if let Some(type_) = caps.get(4) {
+                    out.push_str("{:");
+                    if let Some(flag) = caps.get(1) {
+                        out.push_str(flag.as_str());
                     }
+                    if let Some(width) = caps.get(2) {
+                        out.push_str(width.as_str());
+                    }
+                    if let Some(precision) = caps.get(3) {
+                        out.push_str(precision.as_str());
+                    }
+                    match type_.as_str() {
+                        x @ "x" | x @ "X" => out.push_str(x),
+                        _ => {}
+                    }
+                    out.push_str("}");
+                } else if &caps[0] == "%%" {
+                    out.push_str("%");
+                } else {
+                    out.push_str(&caps[0]);
                 }
+            }
 
-                // parse the language and region, then push the match arm
-                let caps = re_lang.captures(lang.as_str()).expect("lang can be parsed");
-                let lang = caps
-                    .get(1)
-                    .expect("the language is always there")
-                    .as_str()
-                    .to_camel_case();
-                let region = caps
-                    .get(3)
-                    .map(|x| format!("{:?}", x.as_str()));
-                let no_region = "_".to_string();
-                match_arms.push((format!(
+            // parse the language and region, then push the match arm
+            let caps = re_lang.captures(lang.as_str()).expect("lang can be parsed");
+            let lang = caps
+                .get(1)
+                .expect("the language is always there")
+                .as_str()
+                .to_camel_case();
+            let region = caps.get(3).map(|x| format!("{:?}", x.as_str()));
+            let no_region = "_".to_string();
+            match_arms.push((
+                format!(
                     "$crate::Lang::{}({}) => format!({:?} $(, $fmt_args)*),\n",
                     lang,
                     region.as_ref().unwrap_or(&no_region),
                     out,
-                ), region.is_some()));
-                all_languages.insert(lang);
-            }
-            match_arms.sort_unstable_by_key(|(_, has_region)| !has_region);
+                ),
+                region.is_some(),
+            ));
+            all_languages.insert(lang);
+        }
+        match_arms.sort_unstable_by_key(|(_, has_region)| !has_region);
 
-            match_arms
-        };
+        match_arms
+    };
 
-        let mut map = HashMap::new();
+    let mut map = HashMap::new();
 
-        // read all the INI files (might override existing keys)
-        $(
-        let file_path = $ini_files;
-        match $crate::read_twine_ini(file_path) {
-            Err(err) => panic!("could not read Twine INI file `{}`: {}", file_path, err),
+    // read all the INI files (might override existing keys)
+    for file_path in ini_files {
+        let file_path = file_path.as_ref();
+        match read_twine_ini(file_path) {
+            Err(err) => panic!(
+                "could not read Twine INI file `{}`: {}",
+                file_path.display(),
+                err
+            ),
             Ok(other_map) => map.extend(other_map),
         }
-        println!("cargo:rerun-if-changed={}", $ini_files);
-        )+
+        println!("cargo:rerun-if-changed={}", file_path.display());
+    }
 
-        let mut src = String::new();
-        let mut all_languages = HashSet::new();
-        src.push_str("#[macro_export]\nmacro_rules! t {\n");
-        for (key, translations) in map {
-            let key = normalize_key(key.as_str());
-            src.push_str(&format!(
-                "({} $(, $fmt_args:expr)* => $lang:expr) => {{\nmatch $lang {{\n", key,
-            ));
+    let mut src = String::new();
+    let mut all_languages = HashSet::new();
+    src.push_str("#[macro_export]\nmacro_rules! t {\n");
+    for (key, translations) in map {
+        let key = normalize_key(key.as_str());
+        src.push_str(&format!(
+            "({} $(, $fmt_args:expr)* => $lang:expr) => {{\nmatch $lang {{\n",
+            key,
+        ));
 
-            let match_arms = generate_match_arms(translations, &mut all_languages);
+        let match_arms = generate_match_arms(translations, &mut all_languages);
 
-            src.extend(match_arms.iter().map(|(match_arm, _)| match_arm.as_str()));
-            src.push_str("}};\n")
-        }
-        src.push_str("}\n");
+        src.extend(match_arms.iter().map(|(match_arm, _)| match_arm.as_str()));
+        src.push_str("}};\n")
+    }
+    src.push_str("}\n");
 
-        // generate the `Lang` enum and its variants
-        src.push_str("#[derive(Debug, Clone, Copy, PartialEq, Hash)]
+    // generate the `Lang` enum and its variants
+    src.push_str(
+        "#[derive(Debug, Clone, Copy, PartialEq, Hash)]
 #[allow(dead_code)]
-pub enum Lang {\n");
-        for lang in all_languages {
-            src.push_str(&format!("{}(&'static str),\n", lang));
-        }
-        src.push_str("}\n");
+pub enum Lang {\n",
+    );
+    for lang in all_languages {
+        src.push_str(&format!("{}(&'static str),\n", lang));
+    }
+    src.push_str("}\n");
 
-        let out_dir = std::env::var_os("OUT_DIR").unwrap();
-        let dest_path = Path::new(&out_dir).join($output_file);
-        let _ = std::fs::create_dir_all(dest_path.parent().unwrap());
-        std::fs::write(&dest_path, src).unwrap();
-    }};
+    let out_dir = std::env::var_os("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join(output_file);
+    let _ = std::fs::create_dir_all(dest_path.parent().unwrap());
+    std::fs::write(&dest_path, src)?;
+
+    Ok(())
 }
 
 pub fn read_twine_ini<P: AsRef<Path>>(
